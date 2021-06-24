adee25c91c1ed323a1621162bb68dfab21b20882
Виктория Якубова <s106597@guu.ru>
2021-06-24 23:41:38 +0300

    Lab 6 is done

---
+++ LR6_1/LR6_1/LR6_1.cpp
@@ -0,0 +1,23 @@
+﻿//Напишите функцию, которая возвращает наибольший из трех целочисленных аргументов;
+#include <iostream>
+using namespace std;
+int max(int a, int b, int c)
+{
+    int m = 0;
+    if ((a > b) && (a > c)) m = a;
+    if ((b > a) && (b > c)) m = b;	//выбор наибольшего числа
+    if ((c > a) && (c > b)) m = c;
+    return m;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b, c;
+    cout << "Введите три числа:" << endl;
+    cin >> a >> b >> c;
+    cout << "Максимальное число=" << max(a, b, c) << endl; //вызов функции
+    system("pause");
+    return 0;
+
+}
+
---
+++ LR6_3/LR6_3/LR6_3.cpp
@@ -0,0 +1,41 @@
+﻿//Реализуйте сортировку массива вставками(массив из целых чисел)
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int m = 10;
+	int a[m];
+	int temp, j;
+	cout << "Исходный массив:" << endl;
+	srand(time(0));
+	for (int i = 0; i < m; i++)
+	{
+
+		a[i] = rand() % 100; //заполнение массива с помощью датчика генерации случайных чисел;
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	for (int i = 1; i < m; i++)
+	{
+		temp = a[i];//вспомогательная переменная; 
+		j = i - 1;
+		while (temp < a[j] && j >= 0) //сортировка по возрастанию. Ее смысл в том, что каждый элемент сравнивается со всеми предыдущими и занимает нужную позицию; 
+		{
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = temp;
+	}
+	cout << "Отсортированный массив:" << endl;
+	for (int i = 0; i < m; i++)
+	{
+		cout << a[i] << " ";
+	}
+	system("pause");
+	return 0;
+}
+
